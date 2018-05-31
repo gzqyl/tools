25bfb285207563238f86b569996e06bb9ef354af
yanping password=hqhq2018 <q@qq.com>
2018-05-31 14:43:21 +0800

    c# post

---
+++ c#POST Request.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+
+            var values = new Dictionary<string, string>
+            {
+               { "name", "hi..." }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            string a = Program.PostXmlResponse("http://virtual_qq.com/p.php", content);
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+
+        public static string PostXmlResponse(string url, FormUrlEncodedContent xmlString)
+
+        {
+          
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage response = httpClient.PostAsync(url, xmlString).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                Task<string> t = response.Content.ReadAsStringAsync();
+                return t.Result;
+            }
+            return string.Empty;
+        }
+    }
+}
